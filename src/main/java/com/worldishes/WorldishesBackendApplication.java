5cf95fb349eeef464d258e84c422090cd9b0b1cf
dia <rdiadama@gmail.com>
2025-03-03 22:45:30 -0500

    init spring boot

---
+++ src/main/java/com/worldishes/WorldishesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.worldishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldishesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldishesBackendApplication.class, args);
+	}
+
+}
